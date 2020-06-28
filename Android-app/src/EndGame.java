703e82a7269b30d74728ef6cbdca84c017af6e75
eenobai <67269852+eenobai@users.noreply.github.com>
2020-06-28 16:22:58 +0300

    added first gui elements

---
+++ Android-app/src/EndGame.java
@@ -0,0 +1,14 @@
+@SuppressWarnings("unchecked")
+public class EndGame extends Main{
+	
+	public static void finalScore() {
+		
+	if(playerAnswers.equals(correctAnswer)) {
+		System.out.println("gg you won");
+	}else{
+		System.out.println("pepehands");
+	}
+		
+	}
+
+}
