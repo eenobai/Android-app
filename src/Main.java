2b731e5ea97c4271cc607b77a3c7ce34bbfcec8f
eenobai <67269852+eenobai@users.noreply.github.com>
2020-06-28 21:34:22 +0300

    tidied up main

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,33 @@
+<<<<<<< HEAD
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
+public class Main{
+	
+	static ArrayList<Integer> imageId = new ArrayList();
+    static ArrayList<String> questionText = new ArrayList();
+    static ArrayList<String> answers = new ArrayList();
+    static ArrayList<Integer> correctAnswer = new ArrayList();
+    static ArrayList<Integer> playerAnswers = new ArrayList();
+    static ArrayList<String> bug = new ArrayList();
+    static ArrayList<String> tempAnswers = new ArrayList();
+
+    public Main() {
+    }
+
+    public static void finishGame() {
+    	EndGame.finalScore();
+    }
+	public static void main(String[] args) throws FileNotFoundException {
+		System.out.println("1-start game, 2 -exit game;");
+		Parser.parser();
+		GUI.menuPanel();
+		/*Scanner sc = new Scanner(System.in);
+		i = sc.nextInt();
+		menu(i);
+		 */
+	}
+=======
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -64,4 +94,5 @@ public class Main{
 
 		 */
 	}
+>>>>>>> 9b1a2bc73e6f52dfb628edae162a078542699099
 }
